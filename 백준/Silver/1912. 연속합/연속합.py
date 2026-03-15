fe9a2ccef24f1cf2d51e1c380bbf2eb1f2f58922
Jooyoung Kim <49812691+jyakler@users.noreply.github.com>
2026-03-15 09:37:32 +0900

    [Silver II] Title: 연속합, Time: 100 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -2,18 +2,9 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 l = list(map(int, input().split()))
-current = 0
-save = 0
-result = -10000
-for i in l:
-    current += i
-    if current > 0 and current > i:
-        # 0보다 크면 일단 keep going
-        save = current
-    else:
-        save = i
-        current = i
-    result = max(result, current, i)
-    # print(f"[{i}]: {save}, result:{result}")
-
+dp = [0] * 100001
+result = -100000
+for i in range(1, n+1):
+    dp[i] = max(dp[i-1]+l[i-1], l[i-1])
+    result = max(result, dp[i])
 print(result)
