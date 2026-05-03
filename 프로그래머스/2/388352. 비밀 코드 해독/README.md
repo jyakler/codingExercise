98e4779c56eeedb304177cc7c8311a54fb599c7b
Jooyoung Kim <49812691+jyakler@users.noreply.github.com>
2026-05-03 11:25:06 +0900

    [level 2] Title: 비밀 코드 해독, Time: 159.53 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/README.md
+++ 프로그래머스/2/388352. 비밀 코드 해독/README.md
@@ -1,10 +1,10 @@
 # [level 2] 비밀 코드 해독 - 388352 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388352?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388352) 
 
 ### 성능 요약
 
-메모리: 9.22 MB, 시간: 258.26 ms
+메모리: 9.32 MB, 시간: 159.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 15일 08:22:17
+2026년 05월 03일 11:25:03
 
 ### 문제 설명
 
--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -2,18 +2,19 @@ from itertools import combinations
 
 def solution(n, q, ans):
     answer = 0
-    combs = combinations(range(1, n+1), 5)
+    combs = combinations(range(1,n+1),5)
     for comb in combs:
-        isAnswer = check(q, comb, ans)
-        if (isAnswer):
-            answer += 1
+        answer+=check(comb,q,ans)
     return answer
 
-def check(q, comb, ans):
-    for i in range(len(q)):
-        common = list(set(q[i]) & set(comb))
-        if len(common) == ans[i]:
-            continue
-        else:
-            return False
-    return True
+
+def check(comb,q,ans):
+    for i in range(len(ans)):
+        target=0
+        for qq in q[i]:
+            if qq in comb:
+                target+=1
+        if target!=ans[i]:
+            return 0
+    return 1
+        
